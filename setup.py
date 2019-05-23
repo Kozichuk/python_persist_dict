783c32742e09cd8e3274efc88f41f3739c91e43d
Kozichuk <yaroslav.kozichuk@yandex.ru>
2019-05-23 20:40:20 +0300

    add setup.py for publishing tool

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="persistence-dict",
+    version="0.0.1a0",
+    author="Yaroslav Kozichuk",
+    description="Dictionary which saving values to filesystem",
+    url="https://github.com/Kozichuk/python_persist_dict",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
