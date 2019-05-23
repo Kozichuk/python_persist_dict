1eb71b48a116f41cb70afa2f506e13ea60e3cd3a
Kozichuk <yaroslav.kozichuk@yandex.ru>
2019-05-23 20:52:56 +0300

    update setup.py - added description;

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ setuptools.setup(
     version="0.0.1a0",
     author="Yaroslav Kozichuk",
     description="Dictionary which saving values to filesystem",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/Kozichuk/python_persist_dict",
     packages=setuptools.find_packages(),
     classifiers=[
