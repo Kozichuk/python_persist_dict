027d444177ec1cbc504d26d5faeccd978af4ff59
Kozichuk <yaroslav.kozichuk@yandex.ru>
2019-05-24 13:48:44 +0300

    remove app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-from persistent_dict.persisted_dict import PersistenceDict
-
-myDictionary = PersistenceDict('persist')
-myDictionary['a'] = {1: [1, 2, 3, 4]}
-print(myDictionary.keys())
-
-print(myDictionary['a'])
-myDictionary.clear()
